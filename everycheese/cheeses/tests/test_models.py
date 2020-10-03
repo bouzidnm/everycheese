aa66f68092bd574380fb79ce2db7e207abb0fe93
bouzidnm <sima.bouzid@invitae.com>
2020-10-03 16:48:04 -0700

    Added a test for Cheese __str__ method

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+pytestmark = pytest.mark.django_db
+
+from ..models import Cheese
+
+def test__str__():
+    cheese = Cheese.objects.create(
+        name = "Stracchino",
+        description = "A semi-sweet, soft Italian cheese, like cream cheese. Goes well with starches.",
+        firmness = Cheese.Firmness.SOFT
+    )
+    
+    assert cheese.__str__() == "Stracchino"
+    assert str(cheese) == "Stracchino"
