82141286c78ae0d54463c53261b33ed154973270
bouzidnm <sima.bouzid@invitae.com>
2020-10-04 10:35:09 -0700

    Added cheese detail templates

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 app_name = "cheeses" # Django wiring to link url to cheeses app
 urlpatterns = [
-    path( route='',
+    path( route='', # be explicit here
         view=views.CheeseListView.as_view(),
-        name='list' #
+        name='list'
+    ),
+    path( route='<slug:slug>/',
+        view=views.CheeseDetailView.as_view(),
+        name='detail'
     ),
 ]
--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -3,4 +3,8 @@ from django.views.generic import ListView, DetailView
 from .models import Cheese
 
 
-class CheeseListView(ListView): model = Cheese
+class CheeseListView(ListView): 
+    model = Cheese
+
+class CheeseDetailView(DetailView):
+    model = Cheese
