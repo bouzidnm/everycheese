cf6d25fcbca0cfcddd510d53c42e59b23fd790a0
bouzidnm <sima.bouzid@invitae.com>
2020-10-05 06:46:31 -0700

    Added remaining url tests

--- everycheese/cheeses/tests/test_urls.py
+++ everycheese/cheeses/tests/test_urls.py
@@ -21,4 +21,15 @@ def test_list_resolve():
 def test_add_reverse():
     assert reverse('cheeses:add') == '/cheeses/add/new/'
 
-## Add the rest of the tests later
+def test_add_resolve():
+    assert resolve('/cheeses/add/new/').view_name == 'cheeses:add'
+
+def test_detail_reverse(cheese):
+    url = reverse('cheeses:detail', kwargs={'slug': cheese.slug})
+    assert url == f"/cheeses/{cheese.slug}/"
+
+def test_detail_resolve(cheese):
+    url = f"/cheeses/{cheese.slug}/"
+    assert resolve(url).view_name == 'cheeses:detail'
+    
+     
